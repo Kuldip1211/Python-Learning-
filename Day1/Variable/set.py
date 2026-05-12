c14c71d51714a9cabfd4aecf12c9fa080c77be33
Kuldeep Chudasma <kuldeepchudasama6999@gmail.com>
2026-05-12 11:47:26 +0530

    first commit

---
+++ Day1/Variable/set.py
@@ -0,0 +1,15 @@
+# Set Data Type
+
+#Set is an unordered collection of data types that is iterable, mutable, and has no duplicate elements. The order of elements in a set is undefined though it may consist of various elements.
+
+# Create a Set in Python
+
+# initializing empty set 
+s1 =  set();
+
+# assign value in set 
+s2 = set(["G1", "g2" , "g3"]);
+
+print(s2);  #output :- {'g3', 'g2', 'G1'}
+
+# Duplicates are removed automatically
